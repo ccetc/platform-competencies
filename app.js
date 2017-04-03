9e3e4d2ab01182ada86ea91c6567373b5282beba
Greg Kops <greg@thinktopography.com>
2017-04-03 00:25:43 -0400

    migrating competencies to new repository

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: 'Competencies',
+  short_description: 'Manage resources required for various job positions',
+  long_description: 'Manage resources required for various job positions',
+  icon: 'trophy',
+  version: '1.0.0'
+}
